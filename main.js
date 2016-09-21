a7930548d96d1e87b84ce77f510cf9540aaddada
Daniel Sundberg <dnlsndbrg@gmail.com>
2016-09-21 12:01:35 +0200

    db testing

--- main.js
+++ main.js
@@ -1,3 +1,43 @@
 "use strict";
 
-let memberModel = require("./model/memberModel");
+
+var Sequelize = require("sequelize")
+
+var sequelize = new Sequelize('database', '', '', {
+  host: 'localhost',
+  dialect: 'sqlite',
+
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  },
+
+  // SQLite only
+  storage: 'database.sqlite'
+});
+
+
+var Member = sequelize.define('member', {
+  firstName: {
+    type: Sequelize.STRING,
+    field: 'first_name' // Will result in an attribute that is firstName when user facing but first_name in the database
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    field: 'last_name' // Will result in an attribute that is lastName when user facing but last_name in the database
+  }
+}, {
+  freezeTableName: true // Model tableName will be the same as the model name
+});
+
+//let memberModel = require("./model/memberModel");
+
+
+
+
+
+Member.create({
+    firstName: 'John',
+    lastName: 'Hancock'
+  });
--- model/MemberModel.js
+++ model/MemberModel.js
@@ -1,8 +1,32 @@
-class Member {
-    constructor(firstName, lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-}
+// class Member {
+//     constructor(firstName, lastName) {
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//     }
+// }
+
+
+var Member = sequelize.define('member', {
+  firstName: {
+    type: Sequelize.STRING,
+    field: 'first_name' // Will result in an attribute that is firstName when user facing but first_name in the database
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    field: 'last_name' // Will result in an attribute that is lastName when user facing but last_name in the database
+  }
+}, {
+  freezeTableName: true // Model tableName will be the same as the model name
+});
 
 module.exports = Member;
+
+
+//
+// Member.sync({force: true}).then(function () {
+//   // Table created
+//   return User.create({
+//     firstName: 'John',
+//     lastName: 'Hancock'
+//   });
+// });
