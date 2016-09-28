fefb3247ac636a7abb57d69e295096b1c6bb3532
Daniel Sundberg <dnlsndbrg@gmail.com>
2016-09-28 10:37:53 +0200

    fixes cls()

--- main.js
+++ main.js
@@ -19,78 +19,37 @@ let mainMenuView = new MainMenuView();
 let MainMenuController = require("./controller/MainMenuController");
 let mainMenuController = new MainMenuController(mainMenuView, memberController);
 
-
-db.sync({force:true})
-.then(() => {
-    console.log("cleared db")
-    let memberData1 = {
-        firstName: "Olof",
-        lastName: "Bolof",
-        personalNumber: "6303221234"
-    }
-
-    let memberData2 = {
-        firstName: "Ulf",
-        lastName: "Bulf",
-        personalNumber: "1212121234"
-    }
-
-    let memberData3 = {
-        firstName: "Ellen",
-        lastName: "Nu",
-        personalNumber: "9907011234"
-    }
-
-    // Register 3 members
-    memberController.register(memberData1);
-    memberController.register(memberData2);
-    memberController.register(memberData3);
-
-    // Try to register already registered member
-    memberController.register(memberData1);
-
-})
+//
+// db.sync({force:true})
+// .then(() => {
+//     console.log("cleared db")
+//     let memberData1 = {
+//         firstName: "Olof",
+//         lastName: "Bolof",
+//         personalNumber: "6303221234"
+//     }
+//
+//     let memberData2 = {
+//         firstName: "Ulf",
+//         lastName: "Bulf",
+//         personalNumber: "1212121234"
+//     }
+//
+//     let memberData3 = {
+//         firstName: "Ellen",
+//         lastName: "Nu",
+//         personalNumber: "9907011234"
+//     }
+//
+//     // Register 3 members
+//     memberController.register(memberData1);
+//     memberController.register(memberData2);
+//     memberController.register(memberData3);
+//
+//     // Try to register already registered member
+//     memberController.register(memberData1);
+//
+// })
 
 consoleUtil.cls();
 mainMenuController.viewMainMenu();
-
-//consoleView.showMenu();
-//memberController.viewExtendedList();
-
-//consoleView.showMainMenu();
-// memberController.viewCompactList();
-//
-// .then(() => {
-//     // return MemberModel.create({
-//     //     firstName: "hans",
-//     //     lastName: "fff",node m
-//     //     personalNumber: "0601010202"
-//     // })
-// })
-// .then(() => {
-//     return BoatModel.create({
-//         type: "scooner",
-//         length: 40,
-//         member_id: 2
-//     })
-// })
-// .catch((e) => {
-//     console.log(e);
-//     //console.log(e.errors[0].message)
-// })
-//
-
-
-
-
-// MemberModel.build( {firstName: "Ellen", lastName: "Nuuu"}).sayHello();
-// let ellen = MemberModel.create({
-//         firstName: 'Kungen',
-//         lastName: 'Nu'
-//     }).then((m) => {
-//         console.log(m.$modelOptions.instanceMethods);
-//         m.sayHello();
-//     });
-//
-
-//let memberModel = require("./model/memberModel");
--- util/console.js
+++ util/console.js
@@ -1,8 +1,13 @@
 let readline = require("readline");
 
 function cls() {
+    // readline.cursorTo(process.stdout, 0, 0);
+    // readline.clearScreenDown(process.stdout);
+    var lines = process.stdout.getWindowSize()[1];
+    for(var i = 0; i < lines; i++) {
+        console.log('\r\n');
+    }
     readline.cursorTo(process.stdout, 0, 0);
-    readline.clearScreenDown(process.stdout);
 }
 
 module.exports = {
