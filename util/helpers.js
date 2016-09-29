fd66cb1c707bcfbda063d182a73e982021ef1c45
dnlsndbrg <dnlsndbrg@gmail.com>
2016-09-29 14:07:04 +0200

    helpers.js, adds trimString method

---
+++ util/helpers.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const readline = require("readline");
+
+// Clears screen
+function cls() {
+    var lines = process.stdout.rows;
+    for(var i = 0; i < lines; i++) {
+        console.log('\r\n');
+    }
+    readline.cursorTo(process.stdout, 0, 0);
+}
+
+// fills or shortens a string to a specified length
+function trimString(string, length) {
+    // if string is longer than the length, cut it down
+    if (string.length > length) string = string.slice(0, length - string.length);
+    // is string is shorter, fill it out with
+    if  (string.length < length) string += " ".repeat(length - string.length);
+
+    return string;
+}
+
+module.exports = {
+    cls: cls,
+    trimString: trimString
+}
