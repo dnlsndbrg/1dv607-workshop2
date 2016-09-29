5248bfad4501701862d03297818082774cc16469
dnlsndbrg <dnlsndbrg@gmail.com>
2016-09-29 14:52:26 +0200

    moves verbose list creation to the view

--- controller/MemberController.js
+++ controller/MemberController.js
@@ -23,53 +23,9 @@ class MemberController {
     }
 
     static viewVerboseList() {
-        const MainMenuController = require("./MainMenuController");
         MemberModel.getList()
         .then((memberList) => {
-            let choices = memberList.map((member) => {
-
-                // Make a string of the members boats
-                let boats = member.boats
-                .map(boat => helpers.trimString(boat.dataValues.type, 10) )
-                .reduce((a, b) => { return `${a} ${b}`; }, "");
-
-                let fullName = helpers.trimString(`${member.firstName} ${member.lastName}`, 20);
-                return {
-                    name: `${fullName} ${member.personalNumber}         ${boats}`,
-                    value:  {
-                        callback: function(){},
-                        context: {}
-                    }
-                }
-            });
-
-            // add some separating lines to the menu
-            choices.push(new inquirer.Separator());
-
-            // add Main Menu choice
-            choices.push({
-                value: {
-                    callback: MainMenuController.viewMainMenu,
-                    context: MainMenuController
-                },
-                name: "Main Menu"
-            });
-
-            // add Exit choice
-            choices.push({
-                value: {
-                    callback: function(){ console.log("Good bye"); },
-                    context: this
-                },
-                name: "Exit"
-            });
-
-            // have the view log the menu and wait for input
-            MemberView.logVerboseListAndGetInput(choices)
-            .then(function(choice) {
-                helpers.cls();
-                choice.selected.callback.bind(choice.selected.context)();
-            });
+            MemberView.createVerboseList(memberList);
         });
     }
 
--- view/MemberView.js
+++ view/MemberView.js
@@ -12,14 +12,56 @@ class MemberView {
         });
     }
 
-    static logVerboseList(members) {
-        members.forEach((member) => {
-            console.log(`${member.id}: ${member.firstName} ${member.lastName} - ${member.personalNumber}`);
-            member.boats.forEach((boat) => {
-                console.log(`${boat.type} - ${boat.length}`)
-            });
-            console.log("-----");
+    // static logVerboseList(members) {
+    //     members.forEach((member) => {
+    //         console.log(`${member.id}: ${member.firstName} ${member.lastName} - ${member.personalNumber}`);
+    //         member.boats.forEach((boat) => {
+    //             console.log(`${boat.type} - ${boat.length}`)
+    //         });
+    //         console.log("-----");
+    //     });
+    // }
+
+    static createVerboseList(memberList) {
+        const MainMenuController = require("./../controller/MainMenuController"); // Hacky :(
+
+        let choices = memberList.map((member) => {
+            // Make a string of the members boats
+            let boats = member.boats
+            .map(boat => helpers.trimString(boat.dataValues.type, 10) )
+            .reduce((a, b) => { return `${a} ${b}`; }, "");
+
+            let fullName = helpers.trimString(`${member.firstName} ${member.lastName}`, 20);
+            return {
+                name: `${fullName} ${member.personalNumber}         ${boats}`,
+                value:  {
+                    callback: function(){},
+                    context: {}
+                }
+            }
         });
+
+        choices.push(new inquirer.Separator());
+
+        // add Main Menu choice
+        choices.push({
+            value: {
+                callback: MainMenuController.viewMainMenu,
+                context: MainMenuController
+            },
+            name: "Main Menu"
+        });
+
+        // add Exit choice
+        choices.push({
+            value: {
+                callback: function(){ console.log("Good bye"); },
+                context: this
+            },
+            name: "Exit"
+        });
+
+        MemberView.logVerboseListAndGetInput(choices)
     }
 
     static logVerboseListAndGetInput(choices) {
@@ -27,14 +69,17 @@ class MemberView {
         let personalNumber = helpers.trimString("Personal Number", 20);
         let boats = "Boats     ";
 
-        return inquirer.prompt([
+        inquirer.prompt([
             {
                 type: 'list',
                 name: 'selected',
-                message: 'Verbose Members List\n' + chalk.green(name + personalNumber + boats),
+                message: 'Verbose Members List\n' + chalk.grey(name + personalNumber + boats),
                 choices: choices,
             }
-        ]);
+        ]).then(function(choice) {
+            helpers.cls();
+            choice.selected.callback.bind(choice.selected.context)();
+        });
     }
 }
 
