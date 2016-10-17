e9441782758e86696f8b029dfc3f52977b3a1c52
Christoffer Ekberg <ce222pc@student.lnu.se>
2016-10-17 16:37:15 +0200

    Create empty functions in boat DAL

--- dal/boat.js
+++ dal/boat.js
@@ -4,32 +4,68 @@ const db = require("../database");
 const Promise = require("promise");
 
 function fetchAll() {
-    return new Promise(function(fulfill, reject) {
-        let statement = "SELECT * FROM Member";
+    return new Promise(function(resolve, reject) {
+        let query = "SELECT * FROM Member";
         let members = [];
-        db.all(statement, (err, rows) => {
+        db.all(query, (err, rows) => {
+            if (err) {
+                return reject(err);
+            }
             rows.forEach((row) => {
                 members.push(new Member(row));
             });
-            return fulfill(members);
+            return resolve(members);
         });
     });
 }
 
 function fetchOne(id) {
-    return new Promise(function(fullfill, reject) {
-        let  mysqlQuery = "SELECT * FROM boat WHERE id=$id";
-
-        db.get(mysqlQuery, {$id:id},(err, row) => {
-            if (err)
+    return new Promise(function(resolve, reject) {
+        let  query = "SELECT * FROM boat WHERE id=$id";
+        db.get(query, {$id:id}, (err, row) => {
+            if (err) {
                 return reject(err);
-            return fulfill(row);
+            }
+            return resolve(row);
         });
     })
 }
 
+function fetchByMemberID(memberID) {
+    return new Promise(function(resolve, reject) {
+        let  query = "SELECT * FROM boat WHERE member_id=$memberID";
+        db.get(query, {$memberID:memberID}, (err, row) => {
+            if (err) {
+                return reject(err);
+            }
+            return resolve(row);
+        });
+    })
+}
+
+function create(member) {
+    return new Promise(function(resolve, reject) {
+        let query = "INSERT INTO member (first_name, last_name, personal_number) VALUES ($firstName, $lastName, $personalNumber)";
+        db.run(query, {
+            $firstName: member.firstName,
+            $lastName: member.lastName,
+            $personalNumber: member.personalNumber
+        }, function(err) {
+            if (err) {
+                return reject(err);
+            }
+            member.id = this.lastID;
+            return resolve(member);
+        });
+    });
+}
+
+function update(memberData) {
+
+}
+
 module.exports = {
     fetchAll,
-    fetchByName,
-    fetchOne
+    fetchOne,
+    fetchByMemberID
 };
