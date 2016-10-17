77eea27f273502a9b5910e1f34f0691f33eda0cf
Daniel Sundberg <dnlsndbrg@gmail.com>
2016-10-17 14:51:23 +0200

    fetchByID

--- dal/member.js
+++ dal/member.js
@@ -11,16 +11,29 @@ function fetchAll() {
             rows.forEach((row) => {
                 members.push(new Member(row));
             });
+
+            if (err)
+                return reject(err);
+
             return fulfill(members);
         });
     });
 }
 
 function fetchByName(name) {
-
 }
 
 function fetchByID(id) {
+    return new Promise(function(fullfill, reject) {
+        let  mysqlQuery = "SELECT * FROM member WHERE id=$id"
+
+        db.get(mysqlQuery, {$id:id},(err, row) => {
+            if (err)
+                return reject(err);
+
+            return fulfill(row);
+        });
+    })
 
 }
 
